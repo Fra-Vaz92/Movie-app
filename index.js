13c4aa1c8be1c9fe3c8d779b125f8a20e26d5269
Fra-Vaz92 <francovazza92@gmail.com>
2024-11-05 22:16:37 +0000

    Update index.js

--- index.js
+++ index.js
@@ -148,14 +148,13 @@ app.get('/directors/:name', passport.authenticate('jwt', { session: false }), as
 
 //DELETE movie from favoirte list
 app.delete('/users/:Username/movies/:movieId', passport.authenticate('jwt', { session: false }), async (req, res) => {
-    const movieId = new mongoose.Types.ObjectId(req.params.movieId);
   await Users.findOneAndUpdate(
       {
           Username: req.params.Username,
           FavoriteMovies: req.params.movieId,
       },
       {
-          $pull: { FavoriteMovies: movieId },
+          $pull: { FavoriteMovies: req.params.movieId },
       },
       { new: true }
   )
@@ -273,11 +272,10 @@ app.put('/users/:Username',passport.authenticate('jwt', { session: false }), asy
 
 //CREATE favorite list
 app.post('/users/:Username/movies/:movieId', passport.authenticate('jwt', { session: false }), async (req, res) => {
-    const movieId = new mongoose.Types.ObjectId(req.params.movieId);
   await Users.findOneAndUpdate(
       { Username: req.params.Username },
       {
-          $push: { FavoriteMovies: movieId },
+          $push: { FavoriteMovies: req.params.movieId },
       },
       { new: true }
   )
